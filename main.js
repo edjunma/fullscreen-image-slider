fb7949e0424644b04e17451504f00784775143f4
edjunma <junwenma@buffalo.edu>
2019-03-27 00:01:58 -0400

    Slide functionality, remove, check, get

--- main.js
+++ main.js
@@ -6,6 +6,16 @@ const intervalTime = 5000;
 let slideInterval;
 
 const nextSlide = () => {
+  // Get current class
   const current = document.querySelector(".current");
+  // Remove current class
   current.classList.remove();
+  // Check for next slide
+  if (current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add("current");
+  } else {
+    // Add current to start
+    slides[0].classList.add("current");
+  }
 };
